06652a7b60160ba0bef48cf1f5ee60522206569d
unknown <absaraviswanathan@gmail.com>
2024-03-06 11:58:57 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const userRoutes = require("./backend/routes/userRoutes");
-const videosRoutes = require("./backend/routes/videosRoutes");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/details")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+  });
+const userRoutes = require("./routes/userRoutes");
+const videosRoutes = require("./routes/videosRoutes");
 require("./backend/model/db");
 
 app.use(express.json());
@@ -9,6 +18,38 @@ app.use(express.static("views"));
 
 app.use("/api", userRoutes);
 app.use("/api", videosRoutes);
+
+app.get("/edit/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const details = await User.findById(id);
+    res.render("edit", { User: details });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error Updating the User");
+  }
+});
+
+app.post("/edit/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+      await User.findByIdAndUpdate(id, req.body);
+      res.redirect("/");
+  } catch (err) {
+      console.error("Error editing sports:", err.message);
+      res.redirect(`/edit/${id}` );
+  }
+});
+app.get("/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+      await User.findByIdAndDelete(id);
+      res.redirect("/");
+  } catch (err) {
+      console.error("Error deleting sports detail:", err.message);
+      res.redirect("/");
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
