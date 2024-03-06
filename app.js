3b648388bbfa29d37df2ac075320c12704ec835e
unknown <absaraviswanathan@gmail.com>
2024-03-06 12:29:42 +0530

    admin routes changed

--- app.js
+++ app.js
@@ -19,38 +19,6 @@ app.use(express.static("views"));
 app.use("/api", userRoutes);
 app.use("/api", videosRoutes);
 
-app.get("/edit/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const details = await User.findById(id);
-    res.render("edit", { User: details });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error Updating the User");
-  }
-});
-
-app.post("/edit/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-      await User.findByIdAndUpdate(id, req.body);
-      res.redirect("/");
-  } catch (err) {
-      console.error("Error editing sports:", err.message);
-      res.redirect(`/edit/${id}` );
-  }
-});
-app.get("/delete/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-      await User.findByIdAndDelete(id);
-      res.redirect("/");
-  } catch (err) {
-      console.error("Error deleting sports detail:", err.message);
-      res.redirect("/");
-  }
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
