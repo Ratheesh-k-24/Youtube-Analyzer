a7180c0be15140cd1fa85158633a4793ef775190
Ratheesh-k-24 <ratheeshkarnan007@gmail.com>
2024-03-06 11:45:59 +0530

    LOGIN SIGNUP code added

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
-const userRoutes = require("./backend/routes/userRoutes");
-const videosRoutes = require("./backend/routes/videosRoutes");
-require("./backend/model/db");
+const userRoutes = require("./routes/userRoutes");
+const videosRoutes = require("./routes/videosRoutes");
+require("./model/db");
 
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("views"));
+app.set("view engine", "ejs");
 
 app.use("/api", userRoutes);
 app.use("/api", videosRoutes);
